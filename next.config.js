14ab20ce03d61ce81eaf2f25ea5811f496a0449b
MonishR0106 <monishftsolution0106@gmail.com>
2026-01-19 16:02:39 +0530

    redirect page url

--- next.config.js
+++ next.config.js
@@ -1,13 +1,29 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        // domains: ['theme905-computer-shop.myshopify.com', 'example.com', 'ecomapi.ftdigitalsolutions.org','www.primeabgb.com'],
-    },
+  images: {
+    unoptimized: true,
+    // domains: [
+    //   'theme905-computer-shop.myshopify.com',
+    //   'example.com',
+    //   'ecomapi.ftdigitalsolutions.org',
+    //   'www.primeabgb.com',
+    // ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/products/c",
+        destination: "/shop",
+        permanent: true, // 301 redirect
+      },
+       {
+        source: "/products",
+        destination: "/shop",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
