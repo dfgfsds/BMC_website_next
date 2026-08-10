77e0ff6e018ef75def938b921d9c4ed0ce2dc927
Asif2501 <asifshah4495@gmail.com>
2026-08-10 18:31:06 +0530

    single category seo schema and content updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -135,6 +135,23 @@ export default function App({ Component, pageProps }: AppProps) {
         src="https://checkout.razorpay.com/v1/checkout.js"
         strategy="lazyOnload"
       />
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+      >
+        {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){
+                (c[a].q=c[a].q||[]).push(arguments)
+              };
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script","y075j96ypz");
+          `}
+      </Script>
 
       <Layout>
         {isLoading && <Loading />}
